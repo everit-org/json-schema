954678f8625f1c155a0206932e464b377df8df9a
Bence Eros <ebence88@gmail.com>
2015-11-26 15:14:58 +0100

    added some javadoc to ValidationException#getPointerToViolation()

--- core/src/main/java/org/everit/json/schema/ValidationException.java
+++ core/src/main/java/org/everit/json/schema/ValidationException.java
@@ -162,6 +162,13 @@ public class ValidationException extends RuntimeException {
     return getPointerToViolation() + ": " + super.getMessage();
   }
 
+  /**
+   * A JSON pointer denoting the part of the document which violates the schema. It always points
+   * from the root of the document to the violating data fragment, therefore it always starts with
+   * <code>#</code>.
+   *
+   * @return the JSON pointer
+   */
   public String getPointerToViolation() {
     return pointerToViolation.toString();
   }
