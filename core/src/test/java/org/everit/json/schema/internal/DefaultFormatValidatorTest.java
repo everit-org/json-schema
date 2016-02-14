4b85ceeed053887c4674b498cacbb8fbd142a562
Bence Eros <ebence88@gmail.com>
2016-02-14 20:12:53 +0100

    bumping version number to 1.2.0, adding license headers

--- core/src/test/java/org/everit/json/schema/internal/DefaultFormatValidatorTest.java
+++ core/src/test/java/org/everit/json/schema/internal/DefaultFormatValidatorTest.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright (C) 2011 Everit Kft. (http://www.everit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.everit.json.schema.internal;
 
 import java.util.Optional;
 
-import org.apache.commons.validator.routines.EmailValidator;
 import org.everit.json.schema.Format;
 import org.junit.Assert;
 import org.junit.Test;
@@ -55,8 +69,6 @@ public class DefaultFormatValidatorTest {
   @Test
   public void emailSuccess() {
     assertSuccess("a@b.com", Format.EMAIL);
-    Assert.assertTrue(EmailValidator.getInstance().isValid("a@b.com"));
-    Assert.assertFalse(EmailValidator.getInstance().isValid("a.@b.com"));
   }
 
   @Test
