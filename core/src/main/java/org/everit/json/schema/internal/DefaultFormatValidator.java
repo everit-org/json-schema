e1d1c39012a5b9bfabd88d61b9e25ff1c629d717
Bence Eros <ebence88@gmail.com>
2016-02-14 19:54:19 +0100

    implemented hostname, uri and date-time formats

--- core/src/main/java/org/everit/json/schema/internal/DefaultFormatValidator.java
+++ core/src/main/java/org/everit/json/schema/internal/DefaultFormatValidator.java
@@ -1,6 +1,10 @@
 package org.everit.json.schema.internal;
 
 import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -8,6 +12,7 @@ import org.everit.json.schema.Format;
 import org.everit.json.schema.FormatValidator;
 
 import com.google.common.net.InetAddresses;
+import com.google.common.net.InternetDomainName;
 
 public class DefaultFormatValidator implements FormatValidator {
 
@@ -15,11 +20,42 @@ public class DefaultFormatValidator implements FormatValidator {
 
   private static final int IPV6_LENGTH = 16;
 
+  private static final String DATETIME_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ssXXX";
+
+  private static final String DATETIME_FORMAT_STRING_SECFRAC = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+
   private Optional<InetAddress> asInetAddress(final String subject) {
-    if (InetAddresses.isInetAddress(subject)) {
-      return Optional.of(InetAddresses.forString(subject));
-    } else {
+    try {
+      if (InetAddresses.isInetAddress(subject)) {
+        return Optional.of(InetAddresses.forString(subject));
+      } else {
+        return Optional.empty();
+      }
+    } catch (NullPointerException e) {
       return Optional.empty();
+    }
+  }
+
+  private Optional<String> checkDateTime(final String subject) {
+    try {
+      new SimpleDateFormat(DATETIME_FORMAT_STRING).parse(subject);
+      return Optional.empty();
+    } catch (ParseException e) {
+      try {
+        new SimpleDateFormat(DATETIME_FORMAT_STRING_SECFRAC).parse(subject);
+        return Optional.empty();
+      } catch (ParseException e1) {
+        return Optional.of(String.format("[%s] is not a valid date-time", subject));
+      }
+    }
+  }
+
+  private Optional<String> checkHostname(final String subject) {
+    try {
+      InternetDomainName.from(subject);
+      return Optional.empty();
+    } catch (IllegalArgumentException | NullPointerException e) {
+      return Optional.of(String.format("[%s] is not a valid hostname", subject));
     }
   }
 
@@ -31,6 +67,15 @@ public class DefaultFormatValidator implements FormatValidator {
         .orElse(Optional.of(String.format(failureFormat, subject)));
   }
 
+  private Optional<String> checkURI(final String subject) {
+    try {
+      new URI(subject);
+      return Optional.empty();
+    } catch (URISyntaxException | NullPointerException e) {
+      return Optional.of(String.format("[%s] is not a valid URI", subject));
+    }
+  }
+
   @Override
   public Optional<String> validate(final String subject, final Format format) {
     Objects.requireNonNull(format, "format cannot be null");
@@ -39,6 +84,12 @@ public class DefaultFormatValidator implements FormatValidator {
         return checkIpAddress(subject, IPV4_LENGTH, "[%s] is not a valid ipv4 address");
       case IPV6:
         return checkIpAddress(subject, IPV6_LENGTH, "[%s] is not a valid ipv6 address");
+      case HOSTNAME:
+        return checkHostname(subject);
+      case URI:
+        return checkURI(subject);
+      case DATE_TIME:
+        return checkDateTime(subject);
       default:
         throw new IllegalArgumentException("unsupported format: " + format);
     }
--- core/src/test/java/org/everit/json/schema/internal/DefaultFormatValidatorTest.java
+++ core/src/test/java/org/everit/json/schema/internal/DefaultFormatValidatorTest.java
@@ -26,6 +26,47 @@ public class DefaultFormatValidatorTest {
   }
 
   @Test
+  public void dateTimeFormatFailure() {
+    assertFailure("2015-03-13T11:00:000", Format.DATE_TIME,
+        "[2015-03-13T11:00:000] is not a valid date-time");
+  }
+
+  @Test
+  public void dateTimeSecFracSuccess() {
+    assertSuccess("2015-02-30T11:00:00.111Z", Format.DATE_TIME);
+  }
+
+  @Test
+  public void dateTimeSuccess() {
+    assertSuccess("2015-03-13T11:00:00+00:00", Format.DATE_TIME);
+  }
+
+  @Test
+  public void dateTimeZSuccess() {
+    assertSuccess("2015-02-30T11:00:00Z", Format.DATE_TIME);
+  }
+
+  @Test
+  public void hostnameLengthFailure() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < 256; ++i) {
+      sb.append('a');
+    }
+    String subject = sb.toString();
+    assertFailure(subject, Format.HOSTNAME, "[" + subject + "] is not a valid hostname");
+  }
+
+  @Test
+  public void hostnameNullFailure() {
+    assertFailure(null, Format.HOSTNAME, "[null] is not a valid hostname");
+  }
+
+  @Test
+  public void hostnameSuccess() {
+    assertSuccess("localhost", Format.HOSTNAME);
+  }
+
+  @Test
   public void ipv4Failure() {
     assertFailure("asd", Format.IPV4, "[asd] is not a valid ipv4 address");
   }
@@ -34,6 +75,11 @@ public class DefaultFormatValidatorTest {
   public void ipv4LengthFailure() {
     assertFailure(IPV6_ADDR, Format.IPV4,
         "[2001:db8:85a3:0:0:8a2e:370:7334] is not a valid ipv4 address");
+  }
+
+  @Test
+  public void ipv4NullFailure() {
+    assertFailure(null, Format.IPV4, "[null] is not a valid ipv4 address");
   }
 
   @Test
@@ -52,6 +98,11 @@ public class DefaultFormatValidatorTest {
   }
 
   @Test
+  public void ipv6NullFailure() {
+    assertFailure(null, Format.IPV6, "[null] is not a valid ipv6 address");
+  }
+
+  @Test
   public void ipv6Success() {
     assertSuccess(IPV6_ADDR, Format.IPV6);
   }
@@ -59,6 +110,21 @@ public class DefaultFormatValidatorTest {
   @Test(expected = NullPointerException.class)
   public void nullFormat() {
     new DefaultFormatValidator().validate("asd", null);
+  }
+
+  @Test
+  public void uriFailure() {
+    assertFailure("12 34", Format.URI, "[12 34] is not a valid URI");
+  }
+
+  @Test
+  public void uriNullFailure() {
+    assertFailure(null, Format.URI, "[null] is not a valid URI");
+  }
+
+  @Test
+  public void uriSuccess() {
+    assertSuccess("http://example.org:8080/example.html", Format.URI);
   }
 
 }
