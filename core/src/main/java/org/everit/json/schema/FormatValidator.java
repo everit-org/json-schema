79869a66669f127fe5d038bb6ddd70b77aba6dd5
Bence Erős <ebence88@gmail.com>
2016-03-13 20:04:22 +0100

    adding some javadoc to FormatValidator#validate()

--- core/src/main/java/org/everit/json/schema/FormatValidator.java
+++ core/src/main/java/org/everit/json/schema/FormatValidator.java
@@ -74,6 +74,17 @@ public interface FormatValidator {
     }
   }
 
+  /**
+   * Implementation-specific validation of {@code subject}. If a validation error occurs then
+   * implementations should return a programmer-friendly error message as a String wrapped in an
+   * Optional. If the validation succeeded then {@link Optional#empty() an empty optional} should be
+   * returned.
+   *
+   * @param subject
+   *          the string to be validated
+   * @return an {@code Optional} wrapping the error message if a validation error occured, otherwise
+   *         {@link Optional#empty() an empty optional}.
+   */
   Optional<String> validate(String subject);
 
 }
