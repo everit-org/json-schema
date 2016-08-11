79d2c0c4d61588f8d1b098a1b7f427852dc34288
Bence Erős <ebence88@gmail.com>
2016-08-11 22:42:35 +0200

    fixing typo in NotSchema exception message

--- core/src/main/java/org/everit/json/schema/NotSchema.java
+++ core/src/main/java/org/everit/json/schema/NotSchema.java
@@ -59,7 +59,7 @@ public class NotSchema extends Schema {
     } catch (ValidationException e) {
       return;
     }
-    throw new ValidationException(this, "subject must not be valid agains schema " + mustNotMatch,
+    throw new ValidationException(this, "subject must not be valid against schema " + mustNotMatch,
         "not");
   }
 
