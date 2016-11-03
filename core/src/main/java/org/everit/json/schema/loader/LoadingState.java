3b8df99adb597f2f1008d2b46d36360b28e0fd00
Bence Erős <ebence88@gmail.com>
2016-11-03 22:57:13 +0100

    initials of LoadingState#createSchemaException

--- core/src/main/java/org/everit/json/schema/loader/LoadingState.java
+++ core/src/main/java/org/everit/json/schema/loader/LoadingState.java
@@ -2,7 +2,6 @@ package org.everit.json.schema.loader;
 
 import org.everit.json.schema.FormatValidator;
 import org.everit.json.schema.ReferenceSchema;
-import org.everit.json.schema.Schema;
 import org.everit.json.schema.SchemaException;
 import org.everit.json.schema.loader.internal.TypeBasedMultiplexer;
 import org.json.JSONObject;
@@ -45,6 +44,15 @@ class LoadingState {
         this.id = id;
     }
 
+    LoadingState(SchemaLoader.SchemaLoaderBuilder builder) {
+        this(builder.httpClient,
+                builder.formatValidators,
+                builder.pointerSchemas,
+                builder.getRootSchemaJson(),
+                builder.schemaJson,
+                builder.id);
+    }
+
     <E> void ifPresent(final String key, final Class<E> expectedType,
             final Consumer<E> consumer) {
         if (schemaJson.has(key)) {
@@ -82,6 +90,10 @@ class LoadingState {
 
     Optional<FormatValidator> getFormatValidator(final String format) {
         return Optional.ofNullable(formatValidators.get(format));
+    }
+
+    public SchemaException createSchemaException(String message) {
+        return new SchemaException(message);
     }
 
 }
---
+++ core/src/test/java/org/everit/json/schema/loader/LoadingStateTest.java
@@ -0,0 +1,20 @@
+package org.everit.json.schema.loader;
+
+import org.everit.json.schema.SchemaException;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author erosb
+ */
+public class LoadingStateTest {
+
+    @Test
+    public void testCreateSchemaException() {
+        LoadingState subject = new LoadingState(SchemaLoader.builder().schemaJson(new JSONObject()));
+        SchemaException actual = subject.createSchemaException("message");
+//        assertEquals("#: message", actual.getMessage());
+    }
+}
