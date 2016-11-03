c29208e3fd7fb9b040472afcd3c3c4130c6e3b6b
Bence Erős <ebence88@gmail.com>
2016-11-03 22:56:52 +0100

    new constructors for SchemaException for tracking json pointer to the violating fragment1

--- core/src/main/java/org/everit/json/schema/SchemaException.java
+++ core/src/main/java/org/everit/json/schema/SchemaException.java
@@ -3,6 +3,7 @@ package org.everit.json.schema;
 import java.util.List;
 
 import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
 import static java.util.stream.Collectors.joining;
 
 /**
@@ -15,6 +16,8 @@ public class SchemaException extends RuntimeException {
 
     private static final long serialVersionUID = 5987489689035036987L;
 
+    private final String pointerToViolation;
+
     private static Object typeOfValue(final Object actualValue) {
         return actualValue == null ? "null" : actualValue.getClass().getSimpleName();
     }
@@ -23,23 +26,58 @@ public class SchemaException extends RuntimeException {
         return expectedTypes.stream().map(Class::getSimpleName).collect(joining(", "));
     }
 
-    public SchemaException(final String message) {
+    public SchemaException(String message, String pointerToViolation) {
         super(message);
+        this.pointerToViolation = requireNonNull(pointerToViolation, "pointerToViolation cannot be null");
     }
 
-    public SchemaException(final String key, final Class<?> expectedType, final Object actualValue) {
+    public SchemaException(String key,
+            Class<?> expectedType,
+            Object actualValue,
+            String pointerToViolation) {
         this(format("key %s : expected type: %s , found : %s", key, expectedType
-                .getSimpleName(), typeOfValue(actualValue)));
+                .getSimpleName(), typeOfValue(actualValue)), pointerToViolation);
+    }
+
+    public SchemaException(String message, Throwable cause, String pointerToViolation) {
+        super(message, cause);
+        this.pointerToViolation = pointerToViolation;
     }
 
     public SchemaException(final String key, final List<Class<?>> expectedTypes,
+            final Object actualValue, String pointerToViolation) {
+        this(format("key %s: expected type is one of %s, found: %s",
+                key, joinClassNames(expectedTypes), typeOfValue(actualValue)), pointerToViolation);
+    }
+
+    @Deprecated
+    public SchemaException(final String message) {
+        this(message, "");
+    }
+
+    @Deprecated
+    public SchemaException(final String key, final Class<?> expectedType, final Object actualValue) {
+        this(format("key %s : expected type: %s , found : %s", key, expectedType
+                .getSimpleName(), typeOfValue(actualValue)), "");
+    }
+
+    @Deprecated
+    public SchemaException(final String key, final List<Class<?>> expectedTypes,
             final Object actualValue) {
         this(format("key %s: expected type is one of %s, found: %s",
-                key, joinClassNames(expectedTypes), typeOfValue(actualValue)));
+                key, joinClassNames(expectedTypes), typeOfValue(actualValue)), "");
     }
 
+    @Deprecated
     public SchemaException(final String message, final Throwable cause) {
-        super(message, cause);
+        this(message, cause, "");
     }
 
+    public String getErrorMessage() {
+        return getMessage();
+    }
+
+    @Override public String getMessage() {
+        return pointerToViolation + ": " + super.getMessage();
+    }
 }
