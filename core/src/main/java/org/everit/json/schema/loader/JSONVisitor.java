fb73059a01afe5eb0be3e6f4591836fe454ae9f1
Bence Erős <ebence88@gmail.com>
2016-12-18 23:04:49 +0100

    making JSONVisitor non-abstract

--- core/src/main/java/org/everit/json/schema/loader/JSONVisitor.java
+++ core/src/main/java/org/everit/json/schema/loader/JSONVisitor.java
@@ -1,25 +1,28 @@
 package org.everit.json.schema.loader;
 
-import org.json.JSONArray;
-
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
  * @author erosb
  */
-public abstract class JSONVisitor {
+public class JSONVisitor {
 
-    public abstract void visitBoolean(boolean value);
+    public void visitBoolean(boolean value) {
+
+    }
 
     void visitArray(List<JSONTraverser> value) {
         value.forEach(val -> val.accept(this));
     }
 
-    public abstract void visitString(String value);
+    public void visitString(String value) {
 
-    public abstract void visitInteger(Integer value);
+    }
+
+    public void visitInteger(Integer value) {
+        
+    }
 
     public void visitObject(Map<String, JSONTraverser> obj) {
         obj.values().stream().forEach(val -> val.accept(this));
