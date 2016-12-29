c5e6848d1f1324f2d251635cc3293620552b8bef
Bence Erős <ebence88@gmail.com>
2016-12-29 15:55:32 +0100

    stub test for LoadingState

---
+++ core/src/test/java/org/everit/json/schema/loader/LoadingStateTest.java
@@ -0,0 +1,17 @@
+package org.everit.json.schema.loader;
+
+import org.junit.Test;
+
+/**
+ * @author erosb
+ */
+public class LoadingStateTest {
+
+    @Test
+    public void childForString() {
+        // TODO
+    }
+
+
+
+}
