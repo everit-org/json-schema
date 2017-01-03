a1e526d81d2d0698832faea2cb7a9487a0c0e653
Bence Erős <ebence88@gmail.com>
2017-01-03 20:01:03 +0100

    initial impl  of JsonObject

---
+++ core/src/main/java/org/everit/json/schema/loader/JsonObject.java
@@ -0,0 +1,68 @@
+package org.everit.json.schema.loader;
+
+import org.everit.json.schema.SchemaException;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * @author erosb
+ */
+class JsonObject {
+
+    private final Map<String, Object> storage;
+
+    private final LoadingState ls;
+
+    JsonObject(Map<String, Object> storage, LoadingState ls) {
+        this.storage = requireNonNull(storage, "storage cannot be null");
+        this.ls = requireNonNull(ls, "ls cannot be null");
+    }
+
+    boolean containsKey(String key) {
+        return storage.containsKey(key);
+    }
+
+    public void require(String key, BiConsumer<JsonValue, LoadingState> consumer) {
+        if (storage.containsKey(key)) {
+            LoadingState childState = ls.childFor(key);
+            consumer.accept(JsonValue.of(storage.get(key), childState), childState);
+        } else {
+            throw failureOfMissingKey(key);
+        }
+    }
+
+    public <R> R require(String key, BiFunction<JsonValue, LoadingState, R> fn) {
+        if (storage.containsKey(key)) {
+            LoadingState childState = ls.childFor(key);
+            return fn.apply(JsonValue.of(storage.get(key), childState), childState);
+        } else {
+            throw failureOfMissingKey(key);
+        }
+    }
+
+    private SchemaException failureOfMissingKey(String key) {
+        return ls.createSchemaException(format("required key [%s] not found", key));
+    }
+
+    public void ifPresent(String key, BiConsumer<JsonValue, LoadingState> consumer) {
+        if (storage.containsKey(key)) {
+            LoadingState childState = ls.childFor(key);
+            consumer.accept(JsonValue.of(storage.get(key), childState), childState);
+        }
+    }
+
+    public <R> Optional<R> ifPresent(String key, BiFunction<JsonValue, LoadingState, R> fn) {
+        if (storage.containsKey(key)) {
+            LoadingState childState = ls.childFor(key);
+            return Optional.of(fn.apply(JsonValue.of(storage.get(key), childState), childState));
+        } else {
+            return Optional.empty();
+        }
+    }
+}
---
+++ core/src/test/java/org/everit/json/schema/loader/JsonObjectTest.java
@@ -0,0 +1,108 @@
+package org.everit.json.schema.loader;
+
+import org.everit.json.schema.SchemaException;
+import org.json.JSONObject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author erosb
+ */
+public class JsonObjectTest {
+
+    private Map<String, Object> storage() {
+        Map<String, Object> rval = new HashMap<>();
+        rval.put("a", true);
+        rval.put("b", new JSONObject());
+        return rval;
+    }
+
+    private final LoadingState emptyLs = JsonValueTest.emptyLs;
+
+    @Rule
+    public ExpectedException expExc = ExpectedException.none();
+
+    @Test
+    public void testHasKey() {
+        assertTrue(subject().containsKey("a"));
+    }
+
+    private JsonObject subject() {
+        return new JsonObject(storage(), JsonValueTest.emptyLs);
+    }
+
+    @SuppressWarnings("unchecked")
+    private BiConsumer<JsonValue, LoadingState> mockConsumer() {
+        return (BiConsumer<JsonValue, LoadingState>) mock(BiConsumer.class);
+    }
+
+    @Test
+    public void testRequireWithConsumer() {
+        BiConsumer<JsonValue, LoadingState> consumer = mockConsumer();
+        subject().require("a", consumer);
+        LoadingState lsForPath = emptyLs.childFor("a");
+        verify(consumer).accept(JsonValue.of(true, lsForPath), lsForPath);
+    }
+
+    @Test
+    public void testRequireWithConsumerFailure() {
+        expExc.expect(SchemaException.class);
+        expExc.expectMessage("#: required key [aaa] not found");
+        BiConsumer<JsonValue, LoadingState> consumer = mockConsumer();
+        subject().require("aaa", consumer);
+        verify(consumer, never()).accept(any(), any());
+    }
+
+    @Test
+    public void testRequireWithFunction() {
+        BiFunction<JsonValue, LoadingState, Boolean> fn = (val, ls) -> false;
+        assertFalse(subject().require("a", fn));
+    }
+
+    @Test
+    public void testRequireWithFunctionFailure() {
+        expExc.expect(SchemaException.class);
+        expExc.expectMessage("#: required key [aaa] not found");
+        subject().require("aaa", (val, ls) -> false);
+    }
+
+    @Test
+    public void testIfPresentWithConsumer() {
+        BiConsumer<JsonValue, LoadingState> consumer = mockConsumer();
+        subject().ifPresent("a", consumer);
+        LoadingState lsForPath = emptyLs.childFor("a");
+        verify(consumer).accept(JsonValue.of(true, lsForPath), lsForPath);
+    }
+
+    @Test
+    public void testIfPresentWithConsumerMiss() {
+        BiConsumer<JsonValue, LoadingState> consumer = mockConsumer();
+        subject().ifPresent("aaa", consumer);
+        verify(consumer, never()).accept(any(), any());
+    }
+
+    @Test
+    public void testIfPresentWithFn() {
+        assertEquals(Integer.valueOf(42), subject().ifPresent("a", (obj, ls) -> 42).get());
+    }
+
+    @Test
+    public void testIfPresentWithFnMiss() {
+        assertEquals(Optional.empty(), subject().ifPresent("aaaa", (obj, ls) -> 42));
+    }
+}
