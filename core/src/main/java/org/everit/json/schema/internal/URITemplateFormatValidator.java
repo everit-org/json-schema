70f6eb068e67eb5c8542a65c0a9141f14d45d584
Bence Erős <ebence88@gmail.com>
2017-07-20 00:02:02 +0200

    uri-template implementation

--- core/src/main/java/org/everit/json/schema/internal/URITemplateFormatValidator.java
+++ core/src/main/java/org/everit/json/schema/internal/URITemplateFormatValidator.java
@@ -1,13 +1,23 @@
 package org.everit.json.schema.internal;
 
+import static java.lang.String.format;
+
 import java.util.Optional;
 
 import org.everit.json.schema.FormatValidator;
 
+import com.damnhandy.uri.template.MalformedUriTemplateException;
+import com.damnhandy.uri.template.UriTemplate;
+
 public class URITemplateFormatValidator implements FormatValidator {
 
     @Override public Optional<String> validate(String subject) {
-        throw new UnsupportedOperationException("not yet implemented");
+        try {
+            UriTemplate.fromTemplate(subject);
+            return Optional.empty();
+        } catch (MalformedUriTemplateException e) {
+            return Optional.of(format("[%s] is not a valid URI template", subject));
+        }
     }
 
     @Override public String formatName() {
