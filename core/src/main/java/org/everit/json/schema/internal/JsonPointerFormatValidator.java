c9eabf104b53adad677971b7e0c7a767ced606f5
Bence Erős <ebence88@gmail.com>
2017-07-20 22:03:34 +0200

    fixing "json-pointer" format related tests

--- core/src/main/java/org/everit/json/schema/internal/JsonPointerFormatValidator.java
+++ core/src/main/java/org/everit/json/schema/internal/JsonPointerFormatValidator.java
@@ -10,12 +10,39 @@ import org.json.JSONPointer;
 public class JsonPointerFormatValidator implements FormatValidator {
 
     @Override public Optional<String> validate(String subject) {
+        if ("".equals(subject)) {
+            return Optional.empty();
+        }
         try {
             new JSONPointer(subject);
-            return Optional.empty();
+            if (subject.startsWith("#")) {
+                return failure(subject);
+            }
+            return checkEscaping(subject);
         } catch (IllegalArgumentException e) {
-            return Optional.of(format("[%s] is not a valid JSON pointer", subject));
+            return failure(subject);
         }
+    }
+
+    protected Optional<String> failure(String subject) {
+        return Optional.of(format("[%s] is not a valid JSON pointer", subject));
+    }
+
+    protected Optional<String> checkEscaping(String subject) {
+        for (int i = 0; i < subject.length() - 1; ++i) {
+            char c = subject.charAt(i);
+            if (c == '~') {
+                char next = subject.charAt(i + 1);
+                if (next == '1' || next == '0') {
+                    continue;
+                }
+                return failure(subject);
+            }
+        }
+        if (subject.charAt(subject.length() - 1) == '~') {
+            return failure(subject);
+        }
+        return Optional.empty();
     }
 
     @Override public String formatName() {
--- core/src/test/java/org/everit/json/schema/internal/JsonPointerFormatValidatorTest.java
+++ core/src/test/java/org/everit/json/schema/internal/JsonPointerFormatValidatorTest.java
@@ -1,6 +1,6 @@
 package org.everit.json.schema.internal;
 
-import static org.everit.json.schema.internal.ValidatorTestSupport.assertFailure;
+import static java.lang.String.format;
 import static org.everit.json.schema.internal.ValidatorTestSupport.assertSuccess;
 
 import org.junit.Test;
@@ -10,18 +10,47 @@ public class JsonPointerFormatValidatorTest {
     private final JsonPointerFormatValidator subject = new JsonPointerFormatValidator();
 
     @Test
-    public void uriSuccess() {
-        assertSuccess("#/hello", subject);
-    }
-
-    @Test
     public void stringSuccess() {
         assertSuccess("/hello", subject);
     }
 
     @Test
+    public void root() {
+        assertSuccess("/", subject);
+    }
+
+    @Test
+    public void emptyStringIsValid() {
+        assertSuccess("", subject);
+    }
+
+    @Test
     public void illegalLeadingCharFailure() {
-        assertFailure("aaa", subject, "[aaa] is not a valid JSON pointer");
+        assertFailure("aaa");
+    }
+
+    @Test
+    public void invalidTildeEscape() {
+        assertFailure("/~asd");
+    }
+
+    @Test
+    public void invalidEscapeNum() {
+        assertFailure("/~2");
+    }
+
+    @Test
+    public void trailingTilde() {
+        assertFailure("/foo/bar~");
+    }
+
+    @Test
+    public void uriFragment() {
+        assertFailure("#/");
+    }
+
+    private void assertFailure(String input) {
+        ValidatorTestSupport.assertFailure(input, subject, format("[%s] is not a valid JSON pointer", input));
     }
 
 }
