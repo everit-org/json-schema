ff194eb04fdf9964c39f2524ff3f52c15b9cb980
Bence Erős <ebence88@gmail.com>
2017-12-29 16:14:57 +0100

    stub methods in EarlyFailingFailureReporter

--- core/src/main/java/org/everit/json/schema/EarlyFailingFailureReporter.java
+++ core/src/main/java/org/everit/json/schema/EarlyFailingFailureReporter.java
@@ -1,4 +1,24 @@
 package org.everit.json.schema;
 
-public class EarlyFailingFailureReporter {
+public class EarlyFailingFailureReporter implements ValidationFailureReporter {
+
+    @Override public void failure(String message, String keyword) {
+
+    }
+
+    @Override public void failure(Class<?> expectedType, Object actualValue) {
+
+    }
+
+    @Override public void failure(ValidationException exc) {
+
+    }
+
+    @Override public ValidationException inContextOfSchema(Schema schema, Runnable task) {
+        return null;
+    }
+
+    @Override public void validationFinished() {
+
+    }
 }
