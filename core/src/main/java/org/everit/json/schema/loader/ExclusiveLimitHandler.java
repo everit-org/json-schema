ecf6d81c94559f413886b15e0993e0fb38e02dde
zgyorffi <gyozso@gmail.com>
2017-12-29 11:03:08 +0100

    Use V6ExclusiveLimitHandler in case of DRAFT_7 too.

--- core/src/main/java/org/everit/json/schema/loader/ExclusiveLimitHandler.java
+++ core/src/main/java/org/everit/json/schema/loader/ExclusiveLimitHandler.java
@@ -34,7 +34,8 @@ interface ExclusiveLimitHandler {
     static ExclusiveLimitHandler ofSpecVersion(SpecificationVersion specVersion) {
         switch (specVersion) {
             case DRAFT_4: return new V4ExclusiveLimitHandler();
-            case DRAFT_6:return new V6ExclusiveLimitHandler();
+            case DRAFT_6:
+            case DRAFT_7: return new V6ExclusiveLimitHandler();
             default: throw new RuntimeException("unknown spec version: " + specVersion);
         }
     }
