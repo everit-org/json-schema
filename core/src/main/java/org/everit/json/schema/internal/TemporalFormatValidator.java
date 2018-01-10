cbc73ecb21332934809f4efac64d6153cce07eed
Zsolt Györffi <gyozso@gmail.com>
2018-01-10 22:49:47 +0100

    Improve TemporalFormatValidator
    Null checking for formatter parameter, defensive copy of formatsAccepted and arrange fields.

--- core/src/main/java/org/everit/json/schema/internal/TemporalFormatValidator.java
+++ core/src/main/java/org/everit/json/schema/internal/TemporalFormatValidator.java
@@ -1,30 +1,32 @@
 package org.everit.json.schema.internal;
 
 import org.everit.json.schema.FormatValidator;
-import org.everit.json.schema.Validator;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Base class for date and time format validators
  */
 public class TemporalFormatValidator implements FormatValidator {
-    private DateTimeFormatter formatter;
-    private List<String> formats_accepted;
-
     final static DateTimeFormatter SECONDS_FRACTION_FORMATTER = new DateTimeFormatterBuilder()
             .appendFraction(ChronoField.NANO_OF_SECOND, 1, 9, true)
             .toFormatter();
     static final String ZONE_OFFSET_PATTERN = "XXX";
 
-    TemporalFormatValidator(DateTimeFormatter formatter, List<String> formats_accepted) {
-        this.formatter = formatter;
-        this.formats_accepted = formats_accepted;
+    private DateTimeFormatter formatter;
+    private List<String> formatsAccepted;
+
+    TemporalFormatValidator(DateTimeFormatter formatter, List<String> formatsAccepted) {
+        this.formatter = requireNonNull(formatter, "formatter cannot be null");
+        this.formatsAccepted = new ArrayList<>(formatsAccepted);
     }
 
     @Override
@@ -33,7 +35,7 @@ public class TemporalFormatValidator implements FormatValidator {
             formatter.parse(subject);
             return Optional.empty();
         } catch (DateTimeParseException e) {
-            return Optional.of(String.format("[%s] is not a valid %s. Expected %s", subject, formatName(), formats_accepted));
+            return Optional.of(String.format("[%s] is not a valid %s. Expected %s", subject, formatName(), formatsAccepted));
         }
     }
 }
