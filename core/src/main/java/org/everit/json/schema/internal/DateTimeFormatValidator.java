bad7f98016cc30ad6f774036c23aa8fa4bdee09d
Bence Erős <ebence88@gmail.com>
2018-01-19 21:19:43 +0100

    changing DateTimeFormatValidator to keep up backward-compatibility

--- core/src/main/java/org/everit/json/schema/internal/DateTimeFormatValidator.java
+++ core/src/main/java/org/everit/json/schema/internal/DateTimeFormatValidator.java
@@ -1,15 +1,31 @@
 package org.everit.json.schema.internal;
 
-import com.google.common.collect.ImmutableList;
+import static org.everit.json.schema.internal.TemporalFormatValidator.SECONDS_FRACTION_FORMATTER;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
+import java.util.Optional;
+
+import org.everit.json.schema.FormatValidator;
+
+import com.google.common.collect.ImmutableList;
 
 /**
  * Implementation of the "date-time" format value.
  */
-public class DateTimeFormatValidator extends TemporalFormatValidator {
+public class DateTimeFormatValidator implements FormatValidator {
+
+    private static class Delegate extends TemporalFormatValidator {
+
+        Delegate() {
+            super(FORMATTER, FORMATS_ACCEPTED);
+        }
+
+        @Override public String formatName() {
+            return "date-time";
+        }
+    }
 
     private static final List<String> FORMATS_ACCEPTED = ImmutableList.of(
             "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,9}Z, yyyy-MM-dd'T'HH:mm:ss[+-]HH:mm",
@@ -24,13 +40,15 @@ public class DateTimeFormatValidator extends TemporalFormatValidator {
         final DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
                 .appendPattern(PARTIAL_DATETIME_PATTERN)
                 .appendOptional(SECONDS_FRACTION_FORMATTER)
-                .appendPattern(ZONE_OFFSET_PATTERN);
+                .appendPattern(TemporalFormatValidator.ZONE_OFFSET_PATTERN);
 
         FORMATTER = builder.toFormatter();
     }
 
-    public DateTimeFormatValidator() {
-        super(FORMATTER, FORMATS_ACCEPTED);
+    private Delegate delegate = new Delegate();
+
+    @Override public Optional<String> validate(String subject) {
+        return delegate.validate(subject);
     }
 
     @Override
