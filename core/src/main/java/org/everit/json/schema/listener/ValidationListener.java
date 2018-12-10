a0f79063c0d0f0176b76e2c2e5701e84423c6c52
Bence Erős <ebence88@gmail.com>
2018-12-10 16:06:18 +0100

    changing ValidationListener so that all of its declared methods have a default no-op implementation

--- core/src/main/java/org/everit/json/schema/listener/ValidationListener.java
+++ core/src/main/java/org/everit/json/schema/listener/ValidationListener.java
@@ -6,55 +6,37 @@ package org.everit.json.schema.listener;
 public interface ValidationListener {
 
     ValidationListener NOOP = new ValidationListener() {
-        @Override public void mismatch(MismatchEvent event) {
-        }
-
-        @Override public void combinedSchemaMatch(CombinedSchemaMatchEvent event) {
-        }
-
-        @Override public void combinedSchemaMismatch(CombinedSchemaMismatchEvent event) {
-        }
-
-        @Override public void schemaReferenced(SchemaReferencedEvent event) {
-        }
-
-        @Override public void ifSchemaMatch(ConditionalSchemaMatchEvent event) {
-        }
-
-        @Override public void ifSchemaMismatch(ConditionalSchemaMismatchEvent event) {
-        }
-
-        @Override public void thenSchemaMatch(ConditionalSchemaMatchEvent event) {
-        }
-
-        @Override public void thenSchemaMismatch(ConditionalSchemaMismatchEvent event) {
-        }
-
-        @Override public void elseSchemaMatch(ConditionalSchemaMatchEvent event) {
-        }
-
-        @Override public void elseSchemaMismatch(ConditionalSchemaMismatchEvent event) {
-        }
     };
 
-    void mismatch(MismatchEvent event);
+    default void mismatch(MismatchEvent event) {
+    }
 
-    void combinedSchemaMatch(CombinedSchemaMatchEvent event);
+    default void combinedSchemaMatch(CombinedSchemaMatchEvent event) {
+    }
 
-    void combinedSchemaMismatch(CombinedSchemaMismatchEvent event);
+    default void combinedSchemaMismatch(CombinedSchemaMismatchEvent event) {
+    }
 
-    void schemaReferenced(SchemaReferencedEvent event);
+    default void schemaReferenced(SchemaReferencedEvent event) {
+    }
 
-    void ifSchemaMatch(ConditionalSchemaMatchEvent event);
+    default void ifSchemaMatch(ConditionalSchemaMatchEvent event) {
+    }
 
-    void ifSchemaMismatch(ConditionalSchemaMismatchEvent event);
+    default void ifSchemaMismatch(ConditionalSchemaMismatchEvent event) {
+    }
 
-    void thenSchemaMatch(ConditionalSchemaMatchEvent event);
+    default void thenSchemaMatch(ConditionalSchemaMatchEvent event) {
+    }
 
-    void thenSchemaMismatch(ConditionalSchemaMismatchEvent event);
+    default void thenSchemaMismatch(ConditionalSchemaMismatchEvent event) {
+    }
 
-    void elseSchemaMatch(ConditionalSchemaMatchEvent event);
+    default void elseSchemaMatch(ConditionalSchemaMatchEvent event) {
 
-    void elseSchemaMismatch(ConditionalSchemaMismatchEvent event);
+    }
+
+    default void elseSchemaMismatch(ConditionalSchemaMismatchEvent event) {
+    }
 }
 
