7dbae50a92279ebd1737e09674755dbb4528a3fd
Taras Ledkov <tledkov@apache.org>
2023-12-15 20:03:20 +0300

    Proposed fix reference that cause StackOverflowException on equals/hashCode (#501)
    exclude the referredSchema from ReferenceSchema.hashCode()-equals() to avoid StackOverflowError.
    
    Co-authored-by: tkledkov <tkledkov@sberbank.ru>

--- core/src/test/java/org/everit/json/schema/ReferenceSchemaTest.java
+++ core/src/test/java/org/everit/json/schema/ReferenceSchemaTest.java
@@ -80,7 +80,7 @@ public class ReferenceSchemaTest {
     public void equalsVerifier() {
         EqualsVerifier.forClass(ReferenceSchema.class)
                 .withRedefinedSuperclass()
-                .withIgnoredFields("schemaLocation", "location")
+                .withIgnoredFields("schemaLocation", "location", "referredSchema")
                 //there are specifically some non final fields for loading of recursive schemas
                 .suppress(Warning.NONFINAL_FIELDS)
                 .suppress(Warning.STRICT_INHERITANCE)
